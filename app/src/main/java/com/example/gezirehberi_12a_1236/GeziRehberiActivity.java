bd8f16c0d4ae3b06a344d4866b304d9c049ecdad
Öğrenci <ogrenci@profiloatl.meb.k12.tr>
2024-02-26 11:45:26 +0300

    dsddll1

---
+++ app/src/main/java/com/example/gezirehberi_12a_1236/GeziRehberiActivity.java
@@ -0,0 +1,14 @@
+package com.example.gezirehberi_12a_1236;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeziRehberiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gezi_rehberi);
+    }
+}
